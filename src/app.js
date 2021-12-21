cee9ef0e0674588dbef5b342477b2fa8d68dfd49
Ranjithkumar Ganesan <ranjithkumar.g@valtech.com>
2021-12-21 14:53:23 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ const forecast = require('./utils/forecast')
 // console.log(path.join(__dirname,'../public'))
 const app = express()
 
+const port = process.env.PORT || 3000;
+
 //Define path for express config
 const pubDirPath = path.join(__dirname,'../public')
 const viewsDirPath = path.join(__dirname, '../templates/views')
@@ -119,6 +121,6 @@ app.get('/home',(req, res)=>{
     res.send('Home Page')
 })
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('Server is up!')
 })
